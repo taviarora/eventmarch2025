5103c81d8313ff62ea4f29dd5a95c6a6ca9bd13d
taviarora <tavinderarora2017@gmail.com>
2025-03-09 19:48:49 +0400

    Accept changes

--- sendEmails.js
+++ sendEmails.js
@@ -116,6 +116,7 @@ function sendEmail(toEmail, message, subject) {
       EMAILJS_SERVICE_ID,
       EMAILJS_TEMPLATE_ID,
       {
+        const filename = "1.png";
         message,
         subject,
 
@@ -123,7 +124,7 @@ function sendEmail(toEmail, message, subject) {
         to_name: "Malika",
         from_email: "tpsarora@gmail.com", // Hardcoded sender's email
         email: "tpsarora@gmail.com", // Send to the recipient email
-        link: "https://taviarora.github.io/eventmarch2025/Birthdays/2.png",
+        link: "https://taviarora.github.io/eventmarch2025/Birthdays/filename",
       },
       {
         publicKey: "qcbXaXrWGMaIRt6_o",
